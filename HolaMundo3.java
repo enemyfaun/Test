6ed30b0b95ebd2f204f2cc3189c82f6d9d1e8dee
gerdoc <ggerdoc@gmail.com>
2021-11-04 07:17:31 -0600

    modificación al programa holamundo3.java

--- HolaMundo3.java
+++ HolaMundo3.java
@@ -3,5 +3,7 @@ public class HolaMundo3
         public void main( String a[])
         {
                 System.out.println( "hola mundo3 " );
+                System.out.println( "hola gerdoc " );
+
         }
 }
